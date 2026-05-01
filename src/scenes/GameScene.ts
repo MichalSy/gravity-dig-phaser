2236a7bd9bd764db16db7b2a8fd23636a9c6ec9a
Aiko <aiko@local>
2026-05-01 21:34:10 +0200

    fix: lighten earthy tunnel transition

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -24,7 +24,7 @@ import { atlasFrameForTile, backwallFrameForTile, tileKey, worldToTile } from '.
 type CursorKeys = Phaser.Types.Input.Keyboard.CursorKeys;
 type Facing = 'east' | 'west';
 
-const GENERATED_ASSET_VERSION = 'tunnel-bedrock-earth-20260501-2125';
+const GENERATED_ASSET_VERSION = 'light-earth-transition-20260501-2134';
 
 export class GameScene extends Phaser.Scene {
   private generator = new GravityDigLevelGenerator();
