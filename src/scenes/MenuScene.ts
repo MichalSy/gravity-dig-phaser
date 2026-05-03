d4dc232811873a06312dcbf688c1c231126edfed
Aiko <aiko@local>
2026-05-03 13:32:25 +0200

    fix: shift title art upward

--- src/scenes/MenuScene.ts
+++ src/scenes/MenuScene.ts
@@ -15,6 +15,7 @@ const MENU_X = 205;
 const MENU_TOP = 608;
 const MENU_BUTTON_SCALE = 0.205;
 const MENU_BUTTON_GAP = 0.14;
+const BACKGROUND_VERTICAL_OFFSET = -0.2;
 
 export class MenuScene extends Phaser.Scene {
   private background!: Phaser.GameObjects.Image;
@@ -59,13 +60,14 @@ export class MenuScene extends Phaser.Scene {
     const width = this.scale.width;
     const height = this.scale.height;
     const sceneScale = width / this.background.width;
-    this.background.setPosition(width / 2, height / 2).setScale(sceneScale);
+    const backgroundCenterY = height / 2 + height * BACKGROUND_VERTICAL_OFFSET;
+    this.background.setPosition(width / 2, backgroundCenterY).setScale(sceneScale);
 
     const buttonScale = sceneScale * MENU_BUTTON_SCALE;
     const buttonHeight = this.textures.get('menu-button-inactive').getSourceImage().height * buttonScale;
     const gap = buttonHeight * MENU_BUTTON_GAP;
     const left = this.backgroundToScreenX(MENU_X, width, sceneScale);
-    const top = this.backgroundToScreenY(MENU_TOP, height, sceneScale);
+    const top = this.backgroundToScreenY(MENU_TOP, backgroundCenterY, sceneScale);
 
     this.buttons.forEach((button, index) => {
       button.image
@@ -80,8 +82,8 @@ export class MenuScene extends Phaser.Scene {
     return screenWidth / 2 + (x - BACKGROUND_WIDTH / 2) * scale;
   }
 
-  private backgroundToScreenY(y: number, screenHeight: number, scale: number): number {
-    return screenHeight / 2 + (y - BACKGROUND_HEIGHT / 2) * scale;
+  private backgroundToScreenY(y: number, backgroundCenterY: number, scale: number): number {
+    return backgroundCenterY + (y - BACKGROUND_HEIGHT / 2) * scale;
   }
 
   private moveSelection(delta: number): void {
