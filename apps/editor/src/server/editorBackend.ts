8a32d42095e596a071796c48cedd681c4524f840
Aiko <aiko@local>
2026-05-11 16:24:33 +0200

    Fix editor dynamic node ref shim

--- apps/editor/src/server/editorBackend.ts
+++ apps/editor/src/server/editorBackend.ts
@@ -1078,6 +1078,8 @@ const prop = {
   number: (value, options = {}) => marker(value, { type: 'Number', ...options }),
   boolean: (value, options = {}) => marker(value, { type: 'Boolean', ...options }),
   assetId: (value, options = {}) => marker(value, { type: 'AssetId', ...options }),
+  nodeRef: (value = null, options = {}) => marker(value, { type: 'NodeRef', ...options }),
+  nodeRefList: (value = [], options = {}) => marker(value, { type: 'NodeRefList', ...options }),
 };
 `;
 }
